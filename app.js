03bd8ae2b82fecb8b5e9cbfb49a2679992eea753
randy <phanr45@gmail.com>
2026-06-21 15:01:37 -0700

    new feature added - need to tweak it a bit, but you can now generate more grids with an input

--- app.js
+++ app.js
@@ -1,15 +1,42 @@
 const divContainer = document.querySelector("#container");
+const generateButton = document.querySelector("#generateGrid");
+
+
 
 for (i = 0; i<256;i++) {
     const div = document.createElement("div");
     div.classList.add("div")
+    div.style.height = '50px'
+    div.style.width = '50px'
+    div.style.border = '1px solid black'
     divContainer.appendChild(div)
     
     div.addEventListener("mouseenter", (e) => {
         div.style.backgroundColor = "red"
     })
 
+    
+
 
 
 }
+
+
+
+generateButton.addEventListener("click",function () {
+    let userInput = prompt("please enter a grid size")
+    for (i = 0; i< userInput ;i++) {
+    const div = document.createElement("div");
+    div.classList.add("div")
+    div.style.height = '50px'
+    div.style.width = '50px'
+    div.style.border = '1px solid black'
+    divContainer.appendChild(div)
+    
+    div.addEventListener("mouseenter", () => {
+        div.style.backgroundColor = "red"
+    })
+    }
+
+})
 
