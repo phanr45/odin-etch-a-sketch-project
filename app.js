39f8935d452bac393b588cbd4256a641b14957a3
randy <phanr45@gmail.com>
2026-06-21 20:48:04 -0700

    new features added - default grid - grid size will not exceed 100 x 100

--- app.js
+++ app.js
@@ -3,26 +3,46 @@ const generateButton = document.querySelector("#generateGrid");
 const gridSize = document.querySelector("#gridSize");
 
 
-
-
-
-
-generateButton.addEventListener("click",function () {
-    divContainer.innerHTML=""
-    let userInput = prompt("please enter a grid size")
-        for (let i = 0; i< userInput * userInput ;i++) {
-        const div = document.createElement("div");
-        const cellsize = 800 / userInput;
-        div.classList.add("div")
-        div.style.height = `${cellsize}px`
-        div.style.width = `${cellsize}px`
-        div.style.backgroundColor = "red"
-        gridSize.textContent = `grid size: ${userInput} x ${userInput}`
-        divContainer.appendChild(div)
-        
+//default grid
+for (i = 1; i < 257; i++) {
+    gridSize.textContent = "default grid size is 16x16"
+    const div = document.createElement("div");
+    div.classList.add("div")
+    div.style.height = '50px'
+    div.style.width = '50px'
+    div.style.backgroundColor = "grey"
+    divContainer.appendChild(div)
     div.addEventListener("mouseenter", () => {
         div.style.backgroundColor = "black"
     })
+
+}
+
+//generate a grid with a prompt
+generateButton.addEventListener("click", function () {
+    divContainer.innerHTML = ""
+    let userInput = prompt("please enter a grid size: ex. 16, 32, 64")
+    for (let i = 0; i < userInput * userInput; i++) {
+        if (userInput >= 100)  {
+            gridSize.textContent = "too large, refresh"
+          
+
+        } else {
+            const div = document.createElement("div");
+            const cellsize = 800 / userInput;
+            div.classList.add("div")
+            div.style.height = `${cellsize}px`
+            div.style.width = `${cellsize}px`
+            div.style.backgroundColor = "grey"
+            gridSize.textContent = `grid size is now  ${userInput} x ${userInput}`
+            divContainer.appendChild(div)
+
+            div.addEventListener("mouseenter", () => {
+                div.style.backgroundColor = "black"
+            })
+        }
+
+
     }
 
 })
